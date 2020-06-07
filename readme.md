49d3b945c7853f5617c182c8666a3b2b34a5d9b6
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2020-06-07 19:42:19 -0300

    Emplementado o localStorage e nativeStorage

---
+++ readme.md
@@ -0,0 +1,2 @@
+foi feito o uso geolocation que retorna a posição do usuário, com esses dados fiz a verificaçao com as pessoas disponiveis no json e adicionei todas em um novo array que contem a pessoa e a distancia. Nesse novo array organizei do menor para o maior e excluir todos acima da terceira posição, retornei para o typeScript e exibi na tela.
+Na tab2 foi utilizado o nativeStorage e localStorage para salvar os dados do usuario
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { NativeStorage } from '@ionic-native/native-storage/ngx'
 import { Pessoa } from '../model/pessoaInterface';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -10,17 +10,55 @@ import { Pessoa } from '../model/pessoaInterface';
 })
 export class Tab2Page {
   pessoaForm: Pessoa = {} as Pessoa;
+  pessoa: Pessoa;
   constructor(
     private native: NativeStorage,
     private pronpt: Platform
   ) { }
 
   ngOnInit() {
-    if (this.pronpt.is('hybrid')) {
-      this.pessoaForm.gender
+    if (localStorage.length != null || this.native.keys() != null) {
+      if (this.pronpt.is('hybrid')) {
+        this.native.getItem("pessoa").then(
+          dado => {
+            this.pessoaForm = dado;
+          }
+        );
+      }
+    } else {
+      this.pessoaForm.email = localStorage.getItem("email");
+      this.pessoaForm.first_name = localStorage.getItem("first_name");
+      this.pessoaForm.last_name = localStorage.getItem("last_name");
+      this.pessoaForm.gender = localStorage.getItem("gender");
+      let aux3: any = localStorage.getItem("latitude");
+      this.pessoaForm.latitude = aux3;
+      aux3 = localStorage.getItem("longitude");
+      this.pessoaForm.longitude = aux3;
     }
   }
-  onSubmit() {
+  logForm(form) {
 
+    if (this.pronpt.is('hybrid')) {
+      this.pessoa = form.value;
+      this.native.setItem("pessoa", { dado: this.pessoa }).then(
+        () => console.log("Dados salvo no NativeStorage"),
+        error => console.log("Error ao salvar no nativeStorage")
+      )
+    } else {
+      this.pessoa = form.value;
+      let aux = this.pessoa.first_name;
+      localStorage.setItem("first_name", aux);
+      aux = this.pessoa.last_name;
+      localStorage.setItem("last_nome", aux);
+      aux = this.pessoa.gender;
+      localStorage.setItem("gender", aux);
+      aux = this.pessoa.email;
+      localStorage.setItem("email", aux);
+      let aux2 = this.pessoa.latitude;
+      localStorage.setItem("latitude", aux);
+      aux2 = this.pessoa.longitude;
+      localStorage.setItem("longitude", aux);
+
+    }
   }
 }
